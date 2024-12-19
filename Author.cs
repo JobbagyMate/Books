b767dcfb01006f302d8053aa1e613dcfb09b2df3
JobbagyMate <jobbagymatte@gmail.com>
2024-12-19 10:04:50 +0100

    Add project files.

---
+++ Author.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    internal class Author
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get => firstName; 
+            set
+            {
+                if (value.Length < 3 ||
+                    value.Length > 32)
+                    throw new Exception("Invalid first name!");
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName; 
+            set
+            {
+                if (value.Length < 3 ||
+                    value.Length > 32)
+                    throw new Exception("Invalid last name!");
+                lastName = value;
+            }
+        }
+        public Guid GUID { get; set; }
+
+        public Author(string fullName)
+        {
+            GUID = Guid.NewGuid();
+            string[] nps = fullName.Split(' ');
+            FirstName = nps[0];
+            LastName = nps[1];
+        }
+    }
+}
---
+++ Book.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    internal class Book
+    {
+        private DateTime year;
+        private string language;
+        private int stock;
+        private int price;
+        private string title;
+        private int year;
+
+        public long ISBN { get; set; }
+        public List<Author> Authors { get; set; }
+        public string Title
+        {
+            get => title; 
+            set
+            {
+                if (value.Length < 3 ||
+                    value.Length > 64) 
+                    throw new Exception("Invalid title!");
+                title = value;
+            }
+        }
+        public int Year
+        {
+            get => year; 
+            set
+            {
+                if (value < 2007 || 
+                    value > DateTime.Today.Year) 
+                    throw new Exception("Invalid year!");
+                year = value;
+            }
+        }
+        public string Language
+        {
+            get => language; 
+            set
+            {
+                if (value != "angol" ||
+                    value != "német" ||
+                    value != "magyar") 
+                    throw new Exception("Invalid language!");
+                language = value;
+            }
+        }
+        public int Stock
+        {
+            get => stock; 
+            set
+            {
+                if (value < 0) 
+                    throw new Exception("Invalid stock value!");
+                stock = value;
+            }
+        }
+        public int Price
+        {
+            get => price; 
+            set
+            {
+                if (value < 1000 || 
+                    value > 10000) 
+                    throw new Exception("Invalid price!");
+                price = value;
+            }
+        }
+
+        public Book(string title, params string[] authorNames)
+        {
+            ISBN = long.Parse(
+                $"{Random.Shared.Next(10000, 1000000)}" +
+                $"{Random.Shared.Next(10000, 1000000)}");
+
+            Title = title;
+
+            List<Author> tempAuts = [];
+            foreach (var an in authorNames Authors.Add(new(an));
+            Authors = tempAuts;
+        }
+
+        public override string ToString()
+        {
+            if (Stock == 0)
+            {
+                _ = $"{Stock} beszerzés alatt";
+            }
+            else
+            {
+                _ = $"{Stock} db";
+            }
+        }
+    }
+}
